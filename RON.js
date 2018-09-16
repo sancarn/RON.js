38e34df91d22a378f7ee93a3d0966ea6203e0a69
Sancarn <sancarn@hotmail.co.uk>
2018-09-16 14:06:57 +0100

    Update RON.js

--- RON.js
+++ RON.js
@@ -1,8 +1,48 @@
+//TODO:
+// Recursive object/hash solution
 (function(){
   class cRON {
     constructor(){
       this.__symbols = {};
     }
+    
+    //Given the current depth and index, determine the new depth and index at sectionString[0]
+    _measureDepth(depth,index,sectionString){
+      switch(sectionString[index]){
+          case "{":
+            depth["{}"]++;
+            depth["total"]++;
+            break;
+          case "}":
+            depth["{}"]--;
+            depth["total"]--;
+            break;
+          case "[":
+            depth["[]"]++;
+            depth["total"]++;
+            break;
+          case "]":
+            depth["[]"]--;
+            depth["total"]--;
+            break;
+          case "#":
+            if(sectionString[index+1] == "<"){
+              depth["#<>"]++;
+              depth["total"]++;
+              index++;
+            }
+            break;
+          case ">":
+            if(sectionString[index-1]!="="){
+              depth["#<>"]--;
+              depth["total"]--;
+            }
+            break;
+      }
+      return [depth,index]
+    }
+    
+    //gets and uses the same symbol determined by passed name
     _getSymbol(name){
       if(!this.__symbols[name]){
         this.__symbols[name] = Symbol(name);
@@ -26,43 +66,14 @@
       }
       var arrayToParse = [];
       //track depth of {} and [] parens
-      var depth = {"[]":0,"{}":0,"#<>":0};
-      var getTotalDepth = ()=>{return Object.values(depth).reduce(((total,num)=>{return total+num;}));};
+      var depth = {"[]":0,"{}":0,"#<>":0, "total":0};
       var sCumulative = "";
       for(var i=0;i<sectionString.length;i++){
+        [depth,i] = this._measureDepth(depth,i,sectionString)
         var char = sectionString[i];
         switch(char){
-          case "{":
-            depth["{}"]++;
-            sCumulative+=char;
-            break;
-          case "}":
-            depth["{}"]--;
-            sCumulative+=char;
-            break;
-          case "[":
-            depth["[]"]++;
-            sCumulative+=char;
-            break;
-          case "]":
-            depth["[]"]--;
-            sCumulative+=char;
-            break;
-          case "#":
-            if(sectionString[i+1] == "<"){
-              depth["#<>"]++;
-              i=i++;
-            }
-            sCumulative+=char;
-            break;
-          case ">":
-            if(sectionString[i-1]!="="){
-              depth["#<>"]--;
-            }
-            sCumulative+=char;
-            break;
           case ",":
-            if(getTotalDepth()==0){
+            if(depth.total==0){
               arrayToParse.push(sCumulative);
               sCumulative="";
             } else {
@@ -70,8 +81,6 @@
             }
             break;
           case "\"":
-          case "'":
-          case "`":
             var to = this._skipString(sectionString.substr(i));
             sCumulative += sectionString.substr(i,to+1);
             i += to;
@@ -90,45 +99,16 @@
       }
       var output = {key:null, value:null};
       //track depth of {} and [] parens
-      var depth = {"[]":0,"{}":0,"#<>":0};
-      var getTotalDepth = ()=>{return Object.values(depth).reduce(((total,num)=>{return total+num;}));};
+      var depth = {"[]":0,"{}":0,"#<>":0, "total":0};
       var sCumulative = "";
       for(var i=0;i<sectionString.length;i++){
+        [depth,i] = this._measureDepth(depth,i,sectionString)
+        
         var char = sectionString[i];
-        var char2 = sectionString[i+1];
         switch(char){
-          case "{":
-            depth["{}"]++;
-            sCumulative+=char;
-            break;
-          case "}":
-            depth["{}"]--;
-            sCumulative+=char;
-            break;
-          case "[":
-            depth["[]"]++;
-            sCumulative+=char;
-            break;
-          case "]":
-            depth["[]"]--;
-            sCumulative+=char;
-            break;
-          case "#":
-            if(sectionString[i+1] == "<"){
-              depth["#<>"]++;
-              i=i++;
-            }
-            sCumulative+=char;
-            break;
-          case ">":
-            if(sectionString[i-1]!="="){
-              depth["#<>"]--;
-            }
-            sCumulative+=char;
-            break;
           case "=":
-            if(char2 == ">"){
-              if(getTotalDepth()==0){
+            if(sectionString[i+1] == ">"){
+              if(depth.total==0){
                 output["key"] = sCumulative;;
                 output["value"] = sectionString.substr(i+2);
                 return output;
@@ -138,8 +118,6 @@
             }
             break;
           case "\"":
-          case "'":
-          case "`":
             var to = this._skipString(sectionString.substr(i));
             sCumulative += sectionString.substr(i,to+1);
             i += to;
@@ -156,6 +134,37 @@
       Klass.prototype = {};
       return new Klass;
     }
+    
+    /*
+     * RON.parse()
+     * The inner workings of this parser are as follows:
+     * * If a string starts and ends with "{" and "}" respectively:
+     *    ** Get the "," seperated list of "Key=>Value" pairs at 0 depth  ##USING this._getList()##
+     *    ** Get the "=>" seperated Key-Value pair as an object where each of the returned strings have a total depth of 0: ##USING this._getKeyValue()##
+     *        *** I.E. Keep in mind some key value pairs will have increased depth E.G. "{:a=>1}=>:something", so one needs to split only at depth == 0
+     *        *** Also keep in mind some key value pairs will have strings which may contain "=>": {"a=>1"=>"b=>c"}
+     *    ** Recursively parse both the key and the value.
+     *    ** Return result
+     * * If a string starts and ends with "[" and "]" respectively:
+     *    ** Get the "," seperated list of values ##USING this._getList()##
+     *    ** Recursively parse each value.
+     *    ** Return result
+     * * If a string starts and ends with "#<" and ">" respectively:
+     *    ** Get the space delimited list at 0 depth of the internals (should be of the form #<ClassName {:vars=>{...},:methods=>[...]}>)
+     *    ** First value is class name
+     *    ** Parse hash and extract :vars (properties) and :methods from hash
+     *    ** Create a new named object.
+     *    ** Assign properties and methods
+     *    ** Return named object
+     * NOW WE ONLY NEED TO DEAL WITH STAND-ALONE TYPES!
+     * * Parse strings taking into account escaped characters: "She said \"hello\" to him"
+     * * Parse symbols (:...) as Symbol()
+     * * Parse Regex /.../... as javascript's regex /.../...
+     * * Parse nil as null
+     * * Parse NaN (Float::NAN) as NaN
+     * * Parse Integers as javascript's integers
+     * * Parse Floats as javascript's floats
+     */
     parse(hash){
       switch(hash[0]){
         case "{":
@@ -190,18 +199,8 @@
           if(hash[1] == "<"){
             //object notation
             if(hash.endsWith(">")){
-              //Assume all objects in standard format produced by this function:
-              /*
-                #Need to add recursive structure checking to this!
-                # ==>  #<ClassName {:vars=>{:@a=>1,:@b=>"abc",...},:methods=>[:someMethod]}>
-                def standardInspection(obj)
-                  data = {}
-                  data[:vars] = obj.instance_variables.map {|var| [var,obj.instance_variable_get(var)]}.to_h
-                  data[:methods] = obj.methods - Object.methods
-                  return "#<#{obj.class.name} #{data.inspect}>"
-                end
-              */
-
+              //Assume all objects in standard RON object form:
+              //#<ClassName {:vars=>{:@a=>1,:@b=>"abc",...},:methods=>[:someMethod]}>
               hash = hash.slice(2,-1);
               var matches = /([^ ]+) (.+)/.exec(hash);
               var customObject = this._createNamedObject(matches[1]);
@@ -251,8 +250,8 @@
           } else {
             throw "Error: Invalid regex.";
           }
-          break;
         case "n":
+          //nil
           if(hash=="nil"){
             return null;
           } else {
@@ -260,6 +259,7 @@
           }
           break;
         case "N":
+          //NaN (Float::NAN)
           if(hash=="NaN"){
             return NaN;
           } else {
@@ -273,6 +273,7 @@
       }
     }
   }
+  //Browser and node support:
   if (typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
     module.exports = new cRON();
   } else {
